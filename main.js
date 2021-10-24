50a5d6b73be2feacc050435cf1b1d7376affbece
Renan Viana <renanviana007@hotmail.com>
2021-10-24 14:19:57 -0300

    class 04

---
+++ main.js
@@ -0,0 +1,34 @@
+// Object Social Media
+const LinksSocialMedia = {
+  github: 'Sr-Renan',
+  youtube: 'channel/UC-9vxQ_lg9jp02elLKv1KhA',
+  facebook: '',
+  instagram: 'renanvianarv',
+  linkedin: 'in/renan-viana-37498b1b0'
+}
+
+//function of setting new social networks.
+function ChangeSocialMediaLinks() {
+  for (let li of SocialLinks.children) {
+    const Social = li.getAttribute('class')
+
+    li.children[0].href = `https://${Social}.com/${LinksSocialMedia[Social]}`
+  }
+}
+
+ChangeSocialMediaLinks()
+
+function GetGutHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      UserName.textContent = data.name
+      UserBio.textContent = data.bio
+      UserLink.href = data.html_url
+      UserPhoto.src = data.avatar_url
+      UserLogin.textContent = data.login
+    })
+}
+GetGutHubProfileInfos()
